cbe14abd24e95db410251287f567615cd12cf943
Adrian Fonseca <adrian2020fm@gmail.com>
2025-07-29 15:25:23 -0500

    Agregado adaptador Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,2 +1,8 @@
-import { defineConfig } from "astro/config";
-export default defineConfig({});
+    import { defineConfig } from 'astro/config';
+    import vercel from '@astrojs/vercel/static'; // o 'serverless' si necesitaras funciones
+
+    export default defineConfig({
+    output: 'static',
+    adapter: vercel(),
+    });
+    // Puedes agregar más configuraciones aquí si es necesario
